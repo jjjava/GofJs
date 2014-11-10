42cfea392630a546971d025d83f83236dbde9a3c
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2014-11-10 13:40:20 -0300

    Gof Js - Revisão OO

---
+++ public_html/OO/planeta.js
@@ -0,0 +1,17 @@
+// Definindo a classe planeta
+function Planeta(nome) {
+
+    // Definindo uma propriedade
+    this.nomePlaneta = nome;
+}
+
+// Iniciando a classe
+var terra = new Planeta("Terra");
+var terra = new Planeta("Jupter");
+var terra = new Planeta("Saturno");
+var terra = new Planeta("Terra");var terra = new Planeta("Terra");
+
+
+
+// Exibindo o nome do planeta no objeto 'terra'
+console.log("O nome do planeta do objeto 'terra' é: "+terra.nomePlaneta);
