f0323824d12990fc99bb14ecbc6880670dc506af
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2014-11-11 06:24:55 -0300

    Gof Js - Revisão OO

--- public_html/OO/planeta.js
+++ public_html/OO/planeta.js
@@ -9,7 +9,8 @@ function Planeta(nome) {
 var terra = new Planeta("Terra");
 var terra = new Planeta("Jupter");
 var terra = new Planeta("Saturno");
-var terra = new Planeta("Terra");var terra = new Planeta("Terra");
+var terra = new Planeta("Terra");
+var terra = new Planeta("Terra");
 
 // Exibindo o nome do planeta no objeto 'terra'
 console.log("O nome do planeta do objeto 'terra' é: "+terra.nomePlaneta);
